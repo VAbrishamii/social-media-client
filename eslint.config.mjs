070365a06224ec79f339d335d9d8ff431a26fb7e
VAbrishamii <vahide.abri@yahoo.com>
2024-10-17 14:34:18 +0200

    test commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
+import cypress from 'eslint-plugin-cypress';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,11 +14,21 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:cypress/recommended',
+  ),
   {
+    plugins: {
+      cypress,
+    },
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        ...globals['cypress/globals'],
       },
       ecmaVersion: 12,
       sourceType: 'module',
